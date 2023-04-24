6460792fb5c66e4b64c2dba2232582ef3fcfba6d
Kaique da Silva <kaeyosthaeron@gmail.com>
2023-04-23 21:05:00 -0300

    Removed unusued tests

--- test/visus_test.dart
+++ test/visus_test.dart
@@ -1,8 +1,8 @@
-import 'package:visus/visus.dart';
-import 'package:test/test.dart';
+// import 'package:visus/visus.dart';
+// import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
+  // test('calculate', () {
+  //   expect(calculate(), 42);
+  // });
 }
